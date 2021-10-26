c19df1e3d807f934fef59d216fcc1f559c6e2412
Aziz <91526733+naftiaziz@users.noreply.github.com>
2021-10-26 10:57:57 +0100

    Update MainActivity.kt

--- app/src/main/java/com/aziz/diceroller/MainActivity.kt
+++ app/src/main/java/com/aziz/diceroller/MainActivity.kt
@@ -2,7 +2,7 @@ package com.aziz.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -19,8 +19,31 @@ class MainActivity : AppCompatActivity() {
     private fun rollDice() {
         val dice = Dice(6)
         val diceRoll = dice.roll()
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+        val diceRoll2 = dice.roll()
+
+
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        val diceImage2: ImageView = findViewById(R.id.imageView2)
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        val drawableResource2 = when (diceRoll2) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
+        diceImage2.setImageResource(drawableResource2)
+        diceImage.contentDescription = diceRoll.toString()
     }
 
     class Dice(val numSides: Int) {
